9c05d2764b89f4841416217132b8c0c8f833c9ea
Fareed-Ahmad7 <fareedahmad1097@gmail.com>
2023-06-13 07:10:42 +0530

    update table ui

--- client/src/Components/question/QuestionsView.jsx
+++ client/src/Components/question/QuestionsView.jsx
@@ -1,55 +1,69 @@
 // import React from 'react'
 import './QuestionsView.css'
+import Button from "react-bootstrap/Button";
 const QuestionsView = () => {
     return (
-        <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>S.NO</th>
-                        <th>Name Of the Fxperiment</th>
-                        <th>Ques-Avaliable</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><b>1</b></td>
-                        <td>Kruskals Algorithm</td>
-                        <td>Design Analysis of Programs
-                            <button> Open Ques </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><b>2</b></td>
-                        <td>Find a subset of a given set whose element sum is equal to a given number
-                        </td>
-                        <td>Design Analysis of Programs
-                            <button> Open Ques </button></td>
-                    </tr>
-                    <tr>
-                        <td><b>3</b></td>
-                        <td>Knapsack problem using greedy method</td>
-                        <td>Design Analysis of Programs
-                            <button>Open Ques</button></td>
-                    </tr>
-                    <tr>
-                        <td><b>4</b></td>
-                        <td>Knapsack problem using greedy method</td>
-                        <td>Design Analysis of Programs
-                            <button>Open Ques</button></td>
-                    </tr>
-                    <tr>
-                        <td><b>5</b></td>
-                        <td>Stack operations</td>
-                        <td>Design Analysis of Programs
-                            <button>Open Ques</button></td>
-                    </tr>
-                </tbody>
-            </table>
-
-
-        </>
-    )
+      <>
+        <table className="question-table">
+          <thead>
+            <tr>
+              <th>S.NO</th>
+              <th>Name Of the Fxperiment</th>
+              <th>Ques-Avaliable</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <b>1</b>
+              </td>
+              <td>Kruskals Algorithm</td>
+              <td>
+                <Button variant="outline-dark">solve</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>2</b>
+              </td>
+              <td>
+                Find a subset of a given set
+              </td>
+              <td>
+                <Button variant="outline-dark">solve</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>3</b>
+              </td>
+              <td>Knapsack problem using greedy method</td>
+              <td>
+                <Button variant="outline-dark">solve</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>4</b>
+              </td>
+              <td>Knapsack problem using greedy method</td>
+              <td>
+                <Button variant="outline-dark">solve</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>5</b>
+              </td>
+              <td>Stack operations</td>
+              <td>
+                <Button variant="outline-dark">solve</Button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    );
 }
 
 export default QuestionsView
