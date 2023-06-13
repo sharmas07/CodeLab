e8d70861d7e25eeba455537e01b54e5a5b10ad2d
Fareed-Ahmad7 <fareedahmad1097@gmail.com>
2023-06-13 06:42:50 +0530

    add onChange method

--- client/src/Components/CodeEditor/CodeEditorPage.jsx
+++ client/src/Components/CodeEditor/CodeEditorPage.jsx
@@ -1,9 +1,24 @@
+import  { useRef } from "react";
 import QuestionsNav from "./QuestionsNav";
 import QuestionText from "./QuestionText";
 import OutputArea from "./OutputArea";
 import Editor from "@monaco-editor/react";
 import Button from "react-bootstrap/Button";
+
 const CodeEditorPage = () => {
+
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    // this is where you can get source code from editor
+    console.log(editorRef.current.getValue());
+  }
+
+
   return (
     <>
       <QuestionsNav />
@@ -13,6 +28,8 @@ const CodeEditorPage = () => {
         width="100%"
         theme="vs-dark"
         defaultLanguage="python"
+        onMount={handleEditorDidMount}
+        onChange={showValue}
       />
       <OutputArea />
 
