943a25fad77e33bf89c03269f16c2ca519e8864d
sharmas07 <valis6435@gmail.com>
2023-06-15 15:30:43 +0530

    fix: removed footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import LandingPage from "./Components/LandingPage";
 import { Routes, Route } from "react-router-dom";
 import QuestionsView from "../src/Components/question/QuestionsView";
 import CodeEditorPage from "../src/Components/CodeEditor/CodeEditorPage";
-import CreateQuestionsPage from "../src/Components/teacher/CreateQuestionsPage";
 
 function App() {
   return (
@@ -20,10 +19,9 @@ function App() {
           path="/code_editor/:name/:testcase"
           element={<CodeEditorPage />}
         />
-        <Route path="/teacher" element={<CreateQuestionsPage />} />
       </Routes>
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
